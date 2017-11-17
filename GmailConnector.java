7f33bd1efdcfeb2c26958353b4c39663e242f2b9
bailinhq <bailinhuangq@hotmail.com>
2017-11-16 19:41:43 -0600

    Update GmailConnector.java

--- GmailConnector.java
+++ GmailConnector.java
@@ -21,141 +21,102 @@ import java.util.*;
 
 public class GmailConnector {
 
-        /** Application name. */
-        private static final String APPLICATION_NAME =
-                "Gmail API Java Quickstart";
+    /**
+     * Application name.
+     */
+    private static final String APPLICATION_NAME =
+            "Gmail API Java Quickstart";
 
-        /** Directory to store user credentials for this application. */
-        private static final java.io.File DATA_STORE_DIR = new java.io.File(
-                System.getProperty("user.home"), ".credentials/gmail-java-quickstart");
+    /**
+     * Directory to store user credentials for this application.
+     */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(
+            System.getProperty("user.home"), ".credentials/gmail-java-quickstart");
 
-        /** Global instance of the {@link FileDataStoreFactory}. */
-        private static FileDataStoreFactory DATA_STORE_FACTORY;
+    /**
+     * Global instance of the {@link FileDataStoreFactory}.
+     */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
 
-        /** Global instance of the JSON factory. */
-        private static final JsonFactory JSON_FACTORY =
-                JacksonFactory.getDefaultInstance();
+    /**
+     * Global instance of the JSON factory.
+     */
+    private static final JsonFactory JSON_FACTORY =
+            JacksonFactory.getDefaultInstance();
 
-        /** Global instance of the HTTP transport. */
-        private static HttpTransport HTTP_TRANSPORT;
+    /**
+     * Global instance of the HTTP transport.
+     */
+    private static HttpTransport HTTP_TRANSPORT;
 
-        /** Global instance of the scopes required by this quickstart.
-         *
-         * If modifying these scopes, delete your previously saved credentials
-         * at ~/.credentials/gmail-java-quickstart
-         */
-        private static final List<String> SCOPES =
-                Arrays.asList(GmailScopes.GMAIL_READONLY);
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * <p>
+     * If modifying these scopes, delete your previously saved credentials
+     * at ~/.credentials/gmail-java-quickstart
+     */
+    private static final List<String> SCOPES =
+            Arrays.asList(GmailScopes.GMAIL_READONLY);
 
-        private Gmail service;
-        static {
-            try {
-                HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-                DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-            } catch (Throwable t) {
-                t.printStackTrace();
-                System.exit(1);
-            }
+    private Gmail service;
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
         }
-
-        public GmailConnector() throws IOException{
-            service = getGmailService();
-        }
-
-        /**
-         * Creates an authorized Credential object.
-         * @return an authorized Credential object.
-         * @throws IOException
-         */
-        public static Credential authorize() throws IOException {
-            // Load client secrets.
-            InputStream in =
-                    Quickstart.class.getResourceAsStream("/client_secret.json");
-            GoogleClientSecrets clientSecrets =
-                    GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-            // Build flow and trigger user authorization request.
-            GoogleAuthorizationCodeFlow flow =
-                    new GoogleAuthorizationCodeFlow.Builder(
-                            HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                            .setDataStoreFactory(DATA_STORE_FACTORY)
-                            .setAccessType("offline")
-                            .build();
-            Credential credential = new AuthorizationCodeInstalledApp(
-                    flow, new LocalServerReceiver()).authorize("user");
-            System.out.println(
-                    "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-            return credential;
-        }
-
-        /**
-         * Build and return an authorized Gmail client service.
-         * @return an authorized Gmail client service
-         * @throws IOException
-         */
-        public static Gmail getGmailService() throws IOException {
-            Credential credential = authorize();
-            return new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-                    .setApplicationName(APPLICATION_NAME)
-                    .build();
-        }
-
-        public List<Email> getSpam(int cantidad) throws IOException{
-            String user = "me";
-            String Query ="in:Spam";
-            ListMessagesResponse response = service.users().messages().list(user).setQ(Query).execute();
-            List<Email> emails = new ArrayList<Email>();
-            List<Message> messages = new ArrayList<Message>();
-            while (response.getMessages() != null) {
-                messages.addAll(response.getMessages());
-                if (response.getNextPageToken() != null) {
-                    String pageToken = response.getNextPageToken();
-                    response = service.users().messages().list(user).setQ(Query)
-                            .setPageToken(pageToken).execute();
-                } else {
-                    break;
-                }
-            }
-            int contador = 0;
-            for (Message message : messages) {
-                Message mensaje = service.users().messages().get(user, message.getId()).setFormat("full").execute();
-                if(mensaje.getPayload().getMimeType().equals("multipart/alternative")) {
-                    emails.add(new Email(mensaje, true));
-                    contador++;
-                }
-                if(contador == cantidad){
-                    break;
-                }
-            }
-            return emails;
-        }
-
-        public List<Email> getNewMail() throws IOException{
-        String user = "me";
-        String Query ="in:UNREAD";
-        ListMessagesResponse response = service.users().messages().list(user).setQ(Query).execute();
-        List<Email> emails = new ArrayList<Email>();
-        List<Message> messages = new ArrayList<Message>();
-        while (response.getMessages() != null) {
-            messages.addAll(response.getMessages());
-            if (response.getNextPageToken() != null) {
-                String pageToken = response.getNextPageToken();
-                response = service.users().messages().list(user).setQ(Query)
-                        .setPageToken(pageToken).execute();
-            } else {
-                break;
-            }
-        }
-        for (Message message : messages) {
-            Message mensaje = service.users().messages().get(user, message.getId()).setFormat("full").execute();
-            emails.add(new Email(mensaje, true));
-        }
-        return emails;
     }
 
-        public List<Email> getNotSpam(int cantidad) throws IOException{
+    public GmailConnector() throws IOException {
+        service = getGmailService();
+    }
+
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @return an authorized Credential object.
+     * @throws IOException
+     */
+    public static Credential authorize() throws IOException {
+        // Load client secrets.
+        InputStream in =
+                Quickstart.class.getResourceAsStream("/client_secret.json");
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                        .setDataStoreFactory(DATA_STORE_FACTORY)
+                        .setAccessType("offline")
+                        .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+                flow, new LocalServerReceiver()).authorize("user");
+        System.out.println(
+                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+        return credential;
+    }
+
+    /**
+     * Build and return an authorized Gmail client service.
+     *
+     * @return an authorized Gmail client service
+     * @throws IOException
+     */
+    public static Gmail getGmailService() throws IOException {
+        Credential credential = authorize();
+        return new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    public List<Email> getSpam(int cantidad) throws IOException {
         String user = "me";
-        String Query ="in:INBOX"; 
+        String Query = "in:Spam";
         ListMessagesResponse response = service.users().messages().list(user).setQ(Query).execute();
         List<Email> emails = new ArrayList<Email>();
         List<Message> messages = new ArrayList<Message>();
@@ -172,11 +133,71 @@ public class GmailConnector {
         int contador = 0;
         for (Message message : messages) {
             Message mensaje = service.users().messages().get(user, message.getId()).setFormat("full").execute();
-            if(mensaje.getPayload().getMimeType().equals("multipart/alternative")) {
+            if (mensaje.getPayload().getMimeType().equals("multipart/alternative")) {
                 emails.add(new Email(mensaje, true));
                 contador++;
             }
-            if (contador == cantidad){
+            if (contador == cantidad) {
+                break;
+            }
+        }
+        return emails;
+    }
+
+    public List<Email> getNewMail(int cantidad) throws IOException {
+        String user = "me";
+        String Query = "in:UNREAD";
+        ListMessagesResponse response = service.users().messages().list(user).setQ(Query).execute();
+        List<Email> emails = new ArrayList<Email>();
+        List<Message> messages = new ArrayList<Message>();
+        while (response.getMessages() != null) {
+            messages.addAll(response.getMessages());
+            if (response.getNextPageToken() != null) {
+                String pageToken = response.getNextPageToken();
+                response = service.users().messages().list(user).setQ(Query)
+                        .setPageToken(pageToken).execute();
+            } else {
+                break;
+            }
+        }
+        int contador = 0;
+        for (Message message : messages) {
+            Message mensaje = service.users().messages().get(user, message.getId()).setFormat("full").execute();
+            if (mensaje.getPayload().getMimeType().equals("multipart/alternative")) {
+                emails.add(new Email(mensaje, true));
+                contador++;
+            }
+            if (contador == cantidad) {
+                break;
+            }
+        }
+        return emails;
+    }
+
+    public List<Email> getNotSpam(int cantidad) throws IOException {
+        String user = "me";
+        String Query = "in:INBOX";
+        ListMessagesResponse response = service.users().messages().list(user).setQ(Query).execute();
+        List<Email> emails = new ArrayList<Email>();
+        List<Message> messages = new ArrayList<Message>();
+        while (response.getMessages() != null) {
+            messages.addAll(response.getMessages());
+            if (response.getNextPageToken() != null) {
+                String pageToken = response.getNextPageToken();
+                response = service.users().messages().list(user).setQ(Query)
+                        .setPageToken(pageToken).execute();
+            } else {
+                break;
+            }
+        }
+        int contador = 0;
+        for (Message message : messages) {
+            Message mensaje = service.users().messages().get(user, message.getId()).setFormat("full").execute();
+            if (mensaje.getPayload().getMimeType().equals("multipart/alternative")) {
+                emails.add(new Email(mensaje, true));
+                contador++;
+            }
+            if (contador == cantidad) {
                 break;
             }
         }
@@ -188,6 +209,8 @@ public class GmailConnector {
         List<Email> emails = conector.getSpam(10);
         System.out.println(emails.get(1).getBody());
         emails = conector.getNotSpam(5);
+        System.out.println(emails.get(1).getBody());
+        emails = conector.getNewMail(5);
         System.out.println(emails.get(1).getBody());
     }
 
