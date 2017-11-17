b76a4a003de5fa1aa27fbe739ca1178d04d36d04
bailinhq <bailinhuangq@hotmail.com>
2017-11-17 02:10:29 -0600

    Update BayesianFilter.java

--- BayesianFilter.java
+++ BayesianFilter.java
@@ -1,226 +1,138 @@
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.repackaged.org.apache.commons.codec.binary.Base64;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.gmail.Gmail;
-import com.google.api.services.gmail.GmailScopes;
-import com.google.api.services.gmail.model.ListMessagesResponse;
-import com.google.api.services.gmail.model.Message;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.*;
-import java.nio.file.Files;
+public class BayesianFilter {
+    private double spamThresold = 0.9;
+    private double spamProb = 0.3;
+    private int trainingSize;
+    private HashMap<String, WordValue> listaNotSpam;
+    private HashMap<String, WordValue> listaSpam;
 
-public class GmailConnector {
+    public BayesianFilter() {
+        listaNotSpam = new HashMap<String, WordValue>();
+        listaSpam = new HashMap<String, WordValue>();
+    }
 
-    /**
-     * Application name.
-     */
-    private static final String APPLICATION_NAME =
-            "Gmail API Java Quickstart";
+    public void training(List<Email> spam, List<Email> notSpam) {
+        setFrequency(spam, notSpam);
+    }
 
-    /**
-     * Directory to store user credentials for this application.
-     */
-    private static final java.io.File DATA_STORE_DIR = new java.io.File(
-            System.getProperty("user.home"), ".credentials/gmail-java-quickstart");
+    public void isSpam(List<Email> newMessages) {
+        for (int i = 0; i < newMessages.size(); i++){
+            double probability = this.calculateBayProb(newMessages.get(i));
+            if(probability > spamThresold){
+                newMessages.get(i).isSpam = true;
+            }
+        }
+    }
 
-    /**
-     * Global instance of the {@link FileDataStoreFactory}.
-     */
-    private static FileDataStoreFactory DATA_STORE_FACTORY;
+    private double calculateBayProb(Email email) {
+        Pattern pattern = Pattern.compile("(?<!\\S)[a-z]+(?!\\S)");
+        Matcher matcher = pattern.matcher(email.getBody());
+        HashMap<String, String> words = new HashMap<String, String>();
+        while (matcher.find()) {
+            String word = email.getBody().substring(matcher.start(), matcher.end());
+            if (!words.containsKey(word)) {
+                words.put(word, word);
+            }
+        }
+        Set<String> keys = words.keySet();
+        double numerator = 1.0;
+        double denominator1 = 1.0;
+        for (String key : keys) {
+            if (listaSpam.get(key)) {
+                numerator = numerator * listaSpam.get(key).getFrequency();
+            }
+            if (listaNotSpam.get(key)) {
+                denominator1 = denominator1 * listaNotSpam.get(key).getFrequency();
+            }
+        }
+        double probability = numerator / (denominator1 + numerator);
+        return probability;
+    }
 
-    /**
-     * Global instance of the JSON factory.
-     */
-    private static final JsonFactory JSON_FACTORY =
-            JacksonFactory.getDefaultInstance();
-
-    /**
-     * Global instance of the HTTP transport.
-     */
-    private static HttpTransport HTTP_TRANSPORT;
-
-    /**
-     * Global instance of the scopes required by this quickstart.
-     * <p>
-     * If modifying these scopes, delete your previously saved credentials
-     * at ~/.credentials/gmail-java-quickstart
-     */
-    private static final List<String> SCOPES =
-            Arrays.asList(GmailScopes.GMAIL_READONLY);
-
-    private Gmail service;
-
-    static {
-        try {
-            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-        } catch (Throwable t) {
-            t.printStackTrace();
-            System.exit(1);
+    private void setFrequency(List<Email> spam, List<Email> notSpam) {
+        double word_counter = 0.0;
+        for (int i = 0; i < spam.size(); i++) {
+            String body = spam.get(i).body;
+            Pattern pattern = Pattern.compile("(?<!\\S)[a-z]+(?!\\S)");
+            Matcher matcher = pattern.matcher(body);
+            while (matcher.find()) {
+                String word = body.substring(matcher.start(), matcher.end());
+                word_counter++;
+                if (!listaSpam.containsKey(word)) {
+                    listaSpam.put(word, new WordValue(word, 0, 0));
+                    listaSpam.get(word).increaseCount();
+                } else {
+                    listaSpam.get(word).increaseCount();
+                }
+            }
+        }
+        Set<String> keys = listaSpam.keySet();
+        for (String key : keys) {
+            int count = listaSpam.get(key).count;
+            double f = count * 1.0 / spam.size() * 1.0;
+            listaSpam.get(key).setFrequency(f);
+            double p = count * 1.0 / word_counter;
+            listaSpam.get(key).setProbabiliy(p);
+        }
+        for (int i = 0; i < notSpam.size(); i++) {
+            String body = notSpam.get(i).body;
+            Pattern pattern = Pattern.compile("(?<!\\S)[a-z]+(?!\\S)");
+            Matcher matcher = pattern.matcher(body);
+            while (matcher.find()) {
+                String word = body.substring(matcher.start(), matcher.end());
+                word_counter++;
+                if (!listaNotSpam.containsKey(word)) {
+                    listaNotSpam.put(word, new WordValue(word, 0, 0));
+                    listaNotSpam.get(word).increaseCount();
+                } else {
+                    listaNotSpam.get(word).increaseCount();
+                }
+            }
+        }
+        Set<String> keys = listaNotSpam.keySet();
+        for (String key : keys) {
+            int count = listaNotSpam.get(key).count;
+            double f = count * 1.0 / spam.size() * 1.0;
+            listaNotSpam.get(key).setFrequency(f);
+            double p = count * 1.0 / word_counter;
+            listaNotSpam.get(key).setProbabiliy(p);
         }
     }
 
-    public GmailConnector() throws IOException {
-        service = getGmailService();
+    public double getSpamThresold() {
+        return spamThresold;
     }
 
-    /**
-     * Creates an authorized Credential object.
-     *
-     * @return an authorized Credential object.
-     * @throws IOException
-     */
-    public static Credential authorize() throws IOException {
-        // Load client secrets.
-        InputStream in =
-                Quickstart.class.getResourceAsStream("/client_secret.json");
-        GoogleClientSecrets clientSecrets =
-                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow =
-                new GoogleAuthorizationCodeFlow.Builder(
-                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                        .setDataStoreFactory(DATA_STORE_FACTORY)
-                        .setAccessType("offline")
-                        .build();
-        Credential credential = new AuthorizationCodeInstalledApp(
-                flow, new LocalServerReceiver()).authorize("user");
-        System.out.println(
-                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-        return credential;
+    public double getSpamProb() {
+        return spamProb;
     }
 
-    /**
-     * Build and return an authorized Gmail client service.
-     *
-     * @return an authorized Gmail client service
-     * @throws IOException
-     */
-    public static Gmail getGmailService() throws IOException {
-        Credential credential = authorize();
-        return new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-
+    public int getTrainingSize() {
+        return trainingSize;
     }
 
-    public List<Email> getSpam(int cantidad) throws IOException {
-        String user = "me";
-        String Query = "in:Spam";
-        ListMessagesResponse response = service.users().messages().list(user).setQ(Query).execute();
-        List<Email> emails = new ArrayList<Email>();
-        List<Message> messages = new ArrayList<Message>();
-        while (response.getMessages() != null) {
-            messages.addAll(response.getMessages());
-            if (response.getNextPageToken() != null) {
-                String pageToken = response.getNextPageToken();
-                response = service.users().messages().list(user).setQ(Query)
-                        .setPageToken(pageToken).execute();
-            } else {
-                break;
-            }
-        }
-        int contador = 0;
-        for (Message message : messages) {
-            Message mensaje = service.users().messages().get(user, message.getId()).setFormat("full").execute();
-            if (mensaje.getPayload().getMimeType().equals("multipart/alternative")) {
-                emails.add(new Email(mensaje, true));
-                contador++;
-            }
-            if (contador == cantidad) {
-                break;
-            }
-        }
-        return emails;
+    public HashMap<String, WordValue> getListaNotSpam() {
+        return listaNotSpam;
     }
 
-    public List<Email> getNewMail(int cantidad) throws IOException {
-        String user = "me";
-        String Query = "in:UNREAD";
-        ListMessagesResponse response = service.users().messages().list(user).setQ(Query).execute();
-        List<Email> emails = new ArrayList<Email>();
-        List<Message> messages = new ArrayList<Message>();
-        while (response.getMessages() != null) {
-            messages.addAll(response.getMessages());
-            if (response.getNextPageToken() != null) {
-                String pageToken = response.getNextPageToken();
-                response = service.users().messages().list(user).setQ(Query)
-                        .setPageToken(pageToken).execute();
-            } else {
-                break;
-            }
-        }
-        int contador = 0;
-        for (Message message : messages) {
-            Message mensaje = service.users().messages().get(user, message.getId()).setFormat("full").execute();
-            if (mensaje.getPayload().getMimeType().equals("multipart/alternative")) {
-                emails.add(new Email(mensaje, true));
-                contador++;
-            }
-            if (contador == cantidad) {
-                break;
-            }
-        }
-        return emails;
+    public HashMap<String, WordValue> getListaSpam() {
+        return listaSpam;
     }
 
-    public List<Email> getNotSpam(int cantidad) throws IOException {
-        String user = "me";
-        String Query = "in:INBOX";
-        ListMessagesResponse response = service.users().messages().list(user).setQ(Query).execute();
-        List<Email> emails = new ArrayList<Email>();
-        List<Message> messages = new ArrayList<Message>();
-        while (response.getMessages() != null) {
-            messages.addAll(response.getMessages());
-            if (response.getNextPageToken() != null) {
-                String pageToken = response.getNextPageToken();
-                response = service.users().messages().list(user).setQ(Query)
-                        .setPageToken(pageToken).execute();
-            } else {
-                break;
-            }
-        }
-        int contador = 0;
-        for (Message message : messages) {
-            Message mensaje = service.users().messages().get(user, message.getId()).setFormat("full").execute();
-            if (mensaje.getPayload().getMimeType().equals("multipart/alternative")) {
-                emails.add(new Email(mensaje, true));
-                contador++;
-            }
-            if (contador == cantidad) {
-                break;
-            }
-        }
-        return emails;
+    public void setSpamThresold(double spamThresold) {
+        this.spamThresold = spamThresold;
     }
 
-    public static void deleteCredentials() throws IOException{
-        Files.deleteIfExists(DATA_STORE_DIR.toPath());
-        //boolean delete = DATA_STORE_DIR.delete();
-    }
-    public static void main(String[] args) throws IOException {
-        GmailConnector conector = new GmailConnector();
-        List<Email> emails = conector.getSpam(10);
-        System.out.println(emails.get(1).getBody());
-        emails = conector.getNotSpam(5);
-        System.out.println(emails.get(1).getBody());
-        emails = conector.getNewMail(5);
-        System.out.println(emails.get(1).getBody());
-        deleteCredentials();
+    public void setSpamProb(double spamProb) {
+        this.spamProb = spamProb;
     }
 
+    public void setTrainingSize(int trainingSize) {
+        this.trainingSize = trainingSize;
+    }
 }
-
-
