c16603317092720d50b1860b48e400d25613369d
Daniel780 <33237812+Daniel780@users.noreply.github.com>
2017-11-17 01:31:38 -0600

    Update DataSaver.java

--- DataSaver.java
+++ DataSaver.java
@@ -1,17 +1,53 @@
 import java.lang.ref.SoftReference;
-import java.util.Hashtable;
+import java.util.HashMap;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
-public class DataSaver {
+public class Ds {
     private String fileName;
     private String filePath;
 
-    public void Save (Hashtable spam,Hashtable notSpam)
+    public void Save (HashMap spam, HashMap notSpam)
     {
 
+        Set<String> keys = spam.keySet();
+        for (String key: keys){
+            String palabra = spam.get(key).toString();
+            String saludo;
+            saludo = (String) palabra;
+            try {
+                File archivo = new File("text.txt");
+                FileWriter escribir = new FileWriter(archivo, true);
+                escribir.write(saludo+"\n");
+                escribir.close();
+            } catch (Exception e) {
+                System.out.println("Error al escribir");
+            }
+        }
     }
     public void load ()
     {
-
+        String texto="";
+        try
+        {
+            FileReader lector=new FileReader("texto.txt");
+            BufferedReader contenido=new BufferedReader(lector);
+            while((texto=contenido.readLine())!=null)
+            {
+                System.out.println(texto);
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error al leer");
+        }
     }
     public void delete ()
     {
