31aa869f89b7f551b51e6fe372e6e644db6467e5
Daniel780 <33237812+Daniel780@users.noreply.github.com>
2017-11-17 11:33:17 -0600

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -7,7 +7,40 @@ public class Menu {
     }
     public void showMenu ()
     {
+boolean seguir= true;
+        Scanner scan= new Scanner(System.in);
+        int num= scan.nextInt();
+        while(seguir != true){
+            System.out.println("1)Configure \n2)Show Data\n3)Get new mail\n4)Train\n5)displayWords\n6)Logout\n7)Log In\n8)exit");
+            switch(num){
+                case 1:
+                    control.configure;
+                    break;
+                case 2:
+                    control.showData;
+                    break;
+                case 3:
+                    control.getNewMail;
+                    break;
+                case 4:
+                    control.train();
+                    break;
+                case 5:
+                    this.displayWords();
+                    break;
+                case 6:
+                    control.logout;
+                    break;
+                case 7:
+                    control.login;
+                    break;
+                case 8:
+                    control.exit;
+                    break;
+                default: break;
 
+            }
+      }
     }
     public void displayWords ()
     {
