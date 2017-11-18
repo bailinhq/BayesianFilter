11ed462b32e7f55d9275576ee4922568cec0973d
Daniel780 <33237812+Daniel780@users.noreply.github.com>
2017-11-17 18:31:47 -0600

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -9,10 +9,10 @@ public class Menu {
     }
     public void showMenu ()
     {
-        boolean seguir= true;
-        Scanner scan= new Scanner(System.in);
+        boolean seguir= false;
         while(seguir != true){
             System.out.println("1)Configure \n2)Show Data\n3)Get new mail\n4)Train\n5)displayWords\n6)Logout\n7)Log In\n8)exit");
+            Scanner scan= new Scanner(System.in);
             int num= scan.nextInt();
             switch(num){
                 case 1:
